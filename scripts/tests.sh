3cfbc325f7bab4568497358623b670586536fb43
Wang,Jeff <wangjeff@baidu.com>
2018-04-16 14:35:55 -0700

    Test here

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -35,8 +35,8 @@ fi
 $sudo $pip install numpy
 $sudo $pip install Flask
 $sudo $pip install Pillow
-$sudo $pip install six
-$sudo $pip install protobuf
+$sudo $pip install six==1.11.0
+$sudo $pip install protobuf==3.5.1
 
 export PYTHONPATH="${core_path}:${python_path}"
 
